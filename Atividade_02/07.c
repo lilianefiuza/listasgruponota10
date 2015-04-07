6199cb3f7225349bbc77b0f6c49f4e985b5b41bc
paulafranca <paulatavaresfranca@gmail.com>
2015-04-07 15:37:44 -0300

    Update 07.c

--- Atividade_02/07.c
+++ Atividade_02/07.c
@@ -32,19 +32,29 @@ float menor(float a, float b, float c){
            return c;
       }
 }
-int main(){
-    float a, b, c, m, n, p; int i;
-    printf("Digite os números a serem ordenados: "); scanf("%f", a); scanf("%f", b); scanf("%f", c);
-    printf("Digite o valor de I: "), scanf("%d", i);
-    m = maior(a, b, c); n = meio(a, b, c); p = menor(a, b, c);
-    if(i == 1){
-         printf("%f%f%f\n", m, n, p);
+void result(float q,float w,float e,int r){
+    if(r == 1){
+         printf("%f %f %f\n", q, w, e);
     }
-    else if(i == 2){
-         printf("%f%f%f\n", p, n, m);
+    else if(r == 2){
+         printf("%f %f %f\n", e, w, q);
     }
     else{
-         printf("%f%f%f\n", n, m, p);
+         printf("%f %f %f\n", w, q, e);
     }
+    
+}
+int main(){
+    float a, b, c, m, n, p; int i;
+    printf("Digite os nÃºmeros a serem ordenados: \n"); 
+    scanf("%f", &a); 
+    scanf("%f", &b); 
+    scanf("%f", &c);
+    printf("Digite o valor de I: \n"); 
+    scanf("%d", &i);
+    m = maior(a, b, c); 
+    n = meio(a, b, c); 
+    p = menor(a, b, c);
+    result(m,n,p,i);
     return 0;
 }
