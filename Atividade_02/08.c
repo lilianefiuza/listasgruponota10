9b398e07cd7e17730e3ca511feca024a5447aa6a
paulafranca <paulatavaresfranca@gmail.com>
2015-04-07 15:56:27 -0300

    Update 08.c

--- Atividade_02/08.c
+++ Atividade_02/08.c
@@ -1,31 +1,55 @@
 #include <stdio.h>
+float soma(){
+    float a, b;
+    printf("Digite os operandos: \n"); 
+    scanf("%f", &a); 
+    scanf("%f", &b);
+    return a+b;    
+}
+float sub(){
+    float a, b;
+    printf("Digite os operandos: \n"); 
+    scanf("%f", &a); 
+    scanf("%f", &b);
+    return a-b;
+}
+float mult(){
+    float a, b;
+    printf("Digite os operandos: \n"); 
+    scanf("%f", &a); 
+    scanf("%f", &b);
+    return a*b;
+}
+float divi(){
+    float a,b;
+    printf("Digite os operandos: \n"); 
+    scanf("%f", &a); 
+    scanf("%f", &b);
+    return a/b;
+}
+float raiz(){
+    float a;
+    printf("Digite o operando: \n"); 
+    scanf("%f", &a);
+    return sqrt(a);
+}
 int main(){
     float a, b, c; int i;
-    printf("Menu de opções:\n1.Somar dois números.\n2.Subtrair dois números\n3.Multiplicar dois números\n4.Dividir dois números\n5.Raiz quadrada de um número\nDigite a opção desejada."); scanf("%d", i);
+    printf("Menu de opÃ§Ãµes:\n1.Somar dois nÃºmeros.\n2.Subtrair dois nÃºmeros\n3.Multiplicar dois nÃºmeros\n4.Dividir dois nÃºmeros\n5.Raiz quadrada de um nÃºmero\nDigite a opÃ§Ã£o desejada."); scanf("%d", i);
     if(i == 1){
-         printf("Digite os operandos: "); scanf("%f", a); scanf("%f", b);
-         c = a + b;
-         printf("%f", c);
+        printf("%f\n", soma(void));
     }
     else if(i == 2){
-         printf("Digite os operandos: "); scanf("%f", a); scanf("%f", b);
-         c = a - b;
-         printf("%f", c);
+         printf("%f\n", sub(void));
     }
     else if(i == 3){
-         printf("Digite os operandos: "); scanf("%f", a); scanf("%f", b);
-         c = a*b;
-         printf("%f", c);
+         printf("%f\n", mult(void));
     }
     else if(i == 4){
-         printf("Digite os operandos: "); scanf("%f", a); scanf("%f", b);
-         c = a/b;
-         printf("%f", c);
+         printf("%f\n", divi(void));
     }
     else if(i == 5){
-         printf("Digite o operando: "); scanf("%f", a);
-         c = sqrt(a);
-         printf("%f", c);
+         printf("%f\n", raiz(void));
     }
     return 0;
 }
