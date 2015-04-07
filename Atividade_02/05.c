03b94df812e48150a018885ce22dd9aa655ca961
paulafranca <paulatavaresfranca@gmail.com>
2015-04-07 15:28:16 -0300

    Update 05.c

--- Atividade_02/05.c
+++ Atividade_02/05.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
-int main(){
-    float a, b, c, d;
-    printf("Digite três números em ordem crescente: "); scanf("%f", a); scanf("%f", b); scanf("%f", c);
-    printf("Digite um número: ");  scanf("%f", d);
+void orgnaiza(float a,float b,float c, float d){
     if (d > a){
-          printf("%f%f%f%f", d, a, b, c);
+          printf("%f %f %f %f", d, a, b, c);
     }
     else if (d > b && d < a){
-          printf("%f%f%f%f", a, d, b, c);
+          printf("%f %f %f %f", a, d, b, c);
     }
     else if (d > c && d < b){
-          printf("%f%f%f%f", a, b, d, c);
+          printf("%f %f %f %f", a, b, d, c);
     }
     else{
-          printf("%f%f%f%f", a, b, c, d);
+          printf("%f %f %f %f", a, b, c, d);
     }
+    printf("\n");
+}
+int main(){
+    float i, j, k, l;
+    printf("Digite trÃªs nÃºmeros em ordem crescente: \n"); 
+    scanf("%f", &i); 
+    scanf("%f", &j); 
+    scanf("%f", &k);
+    printf("Digite um nÃºmero: \n");  
+    scanf("%f", &l);
+    organiza(i,j,k,l);
     return 0;
 }
